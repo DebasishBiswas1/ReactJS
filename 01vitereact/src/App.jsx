4be50ecd51a44ef0ed12346500125e591889ddfd
DebasishBiswas1 <debasishbiswas.apdj@gmail.com>
2024-09-04 11:02:58 +0530

    new jsx file

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Chai from "./chai"
 
 
 function App() {
 
   return (
-    <h1>Vite plus react</h1>
+    <>
+      <h1>Vite plus react</h1>
+      <Chai/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, voluptates.</p>
+    </>
   )
 }
 
