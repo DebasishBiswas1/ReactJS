8e6f20ff1056bfa4dc9ff9fa30e5e28bb4221669
DebasishBiswas1 <debasishbiswas.apdj@gmail.com>
2024-09-15 11:29:21 +0530

    react router done

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React , {useEffect, useState} from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
+    const data = useLoaderData()
+
+    // const [data,setData] = useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // } , [])
     return (
         <div className='text-black bg-white text-center p-4 w-screen text-xl'>
-            Github
+            Github Followers: {data.followers}
+            <img src={data.avatar_url} alt="Git Picture" width={300} />
         </div>
     )
 }
 
 export default Github
+
+export const GithubInfoLoader = async ()=>{
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
