b299d69b63f1bb9a0a08ac04199b48597077bbce
DebasishBiswas1 <debasishbiswas.apdj@gmail.com>
2024-09-16 10:52:17 +0530

    08miniContext add

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Hello Guys!</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
